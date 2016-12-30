aa696e3773fe914b81477ea1d96b61166b6f3725
emesc <tahder77@gmail.com>
2016-12-30 16:32:54 +1100

    Updated links in navigation for logged in users.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,7 +7,7 @@ module SessionsHelper
 
   # returns current logged in user, if any
   def current_user
-    @current_user || = User.find_by(id: sessions[:user_id])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   # returns true if user is logged in, false otherwise
