44135348b199dc8cada1ea89cf45764c2b502d4d
emesc <tahder77@gmail.com>
2017-01-02 20:17:45 +1100

    Added index action and view and admin column to users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def new
     @user = User.new
@@ -36,7 +40,7 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
     end
 
     # before filter
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -38,4 +38,20 @@ class UsersControllerTest < ActionController::TestCase
     assert flash.empty?
     assert_redirected_to root_url
   end
+
+  test "should redirect index when not logged in" do
+    get :index
+    assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { name: @other_user.name, 
+                                            email: @other_user.email,
+                                            password: @other_user.password,
+                                            password_confirmation: @other_user.password_confirmation,
+                                            admin: true }
+    assert_not @other_user.reload.admin?
+  end
 end
