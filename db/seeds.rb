621e9fc135f9db091d2fdb1068685cc17f6fe84a
emesc <tahder77@gmail.com>
2017-01-03 22:22:40 +1100

    Updated seed and fixture users to include activation fields.

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,11 +10,15 @@ User.create!(name: "Example User",
              email: "example@railstutorial.org",
              password: "foobar",
              password_confirmation: "foobar",
-             admin: true)
+             admin: true,
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "foobar"
-  User.create(name: name, email: email, password: password, password_confirmation: password)
+  activated = true
+  activated_at = Time.zone.now
+  User.create(name: name, email: email, password: password, password_confirmation: password, activated: activated, activated_at: activated_at)
 end
